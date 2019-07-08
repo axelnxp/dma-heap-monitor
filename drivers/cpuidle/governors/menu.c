b39a9211df54d3234912e08387ccc1de3cc10213
John Stultz <john.stultz@linaro.org>
2019-07-08 18:58:03 +0000

    Revert "HACK: cpuidle: menu govenor hack from VincentD"
    This reverts commit fd2a6b72b67937a4c0c4e713f74295d804e1d04b.

--- drivers/cpuidle/governors/menu.c
+++ drivers/cpuidle/governors/menu.c
@@ -488,7 +488,6 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 	 * assume the state was never reached and the exit latency is 0.
 	 */
 
-#if 0
 	if (data->tick_wakeup && data->next_timer_us > TICK_USEC) {
 		/*
 		 * The nohz code said that there wouldn't be any events within
@@ -500,9 +499,7 @@ static void menu_update(struct cpuidle_driver *drv, struct cpuidle_device *dev)
 		 * duration predictor do a better job next time.
 		 */
 		measured_us = 9 * MAX_INTERESTING / 10;
-	} else
-#endif
-	if ((drv->states[last_idx].flags & CPUIDLE_FLAG_POLLING) &&
+	} else if ((drv->states[last_idx].flags & CPUIDLE_FLAG_POLLING) &&
 		   dev->poll_time_limit) {
 		/*
 		 * The CPU exited the "polling" state due to a time limit, so
